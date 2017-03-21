0fe796d68aa7e2aa63e20e9f0cd13b4939d558e6
Sherly Chan <sherlyc@gmail.com>
2017-03-21 20:34:05 +1300

    added resetClass

--- dom-events-and-classes/game.js
+++ dom-events-and-classes/game.js
@@ -1,42 +1,54 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls
 // the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+  bindEventListeners(document.getElementsByClassName('board')[0].children);
 }
 
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('click', makeBlue)
-    dots[i].addEventListener('dblclick', hide)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
 
   }
 }
 
 function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+  evt.preventDefault();
+  resetClass (evt);
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
 function makeBlue (evt) {
-  evt.target.classList.toggle('blue')
-  updateCounts()
+  resetClass (evt);
+  evt.target.classList.toggle('blue');
+  updateCounts();
 }
 
 // CREATE FUNCTION hide HERE
 function hide (evt){
-  evt.target.classList.toggle('invisible')
-  updateCounts()
+  resetClass (evt);
+  evt.target.classList.toggle('invisible');
+  updateCounts();
 }
 
+function resetClass (evt){
+    if (evt.target.classList.contains('green')) {
+      evt.target.classList.remove('green');
+    } else if (evt.target.classList.contains('blue')){
+      evt.target.classList.remove('blue');
+    } else if (evt.target.classList.contains('invisible')){
+      evt.target.classList.remove('invisible');
+    }
 
+}
 
 function updateCounts () {
   var totals = {
@@ -46,7 +58,7 @@ function updateCounts () {
   }
 
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-  var arrDots = document.getElementsByClassName('board')[0].children
+  var arrDots = document.getElementsByClassName('board')[0].children;
 
   for (i=0; i< arrDots.length; i++){
     if (arrDots[i].classList.contains('blue')){
@@ -59,11 +71,11 @@ function updateCounts () {
 
   }
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
 function displayTotals (totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
   }
 }
